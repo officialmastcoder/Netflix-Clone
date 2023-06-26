3f3a689fbd8f5ebaf39eb6e45c0d2cffc2cd145c
anuragco <anurag.console@gmail.com>
2023-06-26 09:03:21 +0530

    more pages

--- function/function.js
+++ function/function.js
@@ -2,3 +2,6 @@ function myfunction(){
     document.getElementById('learnmore').innerHTML = "<br>The information collected by Google reCAPTCHA is <br> subject to the Google Privacy Policy and Terms of <br> Service, and is used for providing, maintaining, and <br> improving the reCAPTCHA service and for general <br> security purposes (it is not used for personalised <br> advertising by Google).";
 
 }
+function signout(){
+    "window.location.href='/registration/pass.html'";
+}
